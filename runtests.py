2f00fefe0129d36f4a11158773e67e860a6fa9b8
Erik Groeneveld <erik@lespresverts.com>
2024-02-19 14:57:58 +0100

    works according to idea in README; trying out...

--- runtests.py
+++ runtests.py
@@ -1,57 +1,36 @@
-from clingo import Number, String, Function, Symbol, Control
-import sys, re
+import clingo
+import sys
+import ast
 
-"""
-Supports running test in Answer Set Programming (ASP).
+def parse(o):
+    return o.value if isinstance(o, ast.Constant) else \
+           (o.id, []) if isinstance(o, ast.Name) else \
+           (o.func.id, [parse(a) for a in o.args])
 
-Units to be tested must be designated with #program, for example:
+def parse_signature(s):
+    # parse extended #program syntax using Python's parser
+    return parse(ast.parse(s).body[0].value)
 
-#program generate().
-{ queen(X, Y) :  X = 1..N,  Y = 1..N } = N  :-  board(N).
-
-
-Unit tests are also designated with #program. The name must
-start with 'test_' and the constants indicate depencencies,
-for example:
-
-#program test_generate(generate).
-board(2).
-test(@name(two_of_four)) :- {queen(X,Y) : X=1..2, Y=1..2} = 2.
-
-
-This will ground and solve test_generate with generate. The
-only requirement is that the constants refer to #program's. At runtime
-the constants (generate) have the fixed value 1.
-
-
-Assertions are created by creating and naming an atom called test:
-
-test(@name=<name>) :- <a body>
-
-@name registers the term test(<name>) and checks afterwards if it
-appears in all answer sets. The body is any ASP code you want. The
-name must be unique.
-
-When executed, runtests.py finds all #program test_* and grounds and
-solves them one by one, each time checking if the test(...) terms
-are part of all answers. On first failure, It prints that model and 
-the failed term.
-"""
+p = parse_signature("one(two, three)")
+assert p == ('one', [('two', []), ('three', [])]), p
+p = parse_signature("one(2, 3)")
+assert p == ('one', [2, 3]), p
+p = parse_signature("one(two(2, aap), three(42))")
+assert p == ('one', [('two', [2, ('aap', [])]), ('three', [42])]), p
 
 program_file = sys.argv[1]
-program = open(program_file).read()
 
+# read all the #program parts and register their dependencies
+lines = open(program_file).readlines()
 programs = {}
-
-# read all the #program parts and their dependencies
-for line in program.splitlines():
+for i, line in enumerate(lines):
     if line.startswith('#program'):
-        signature = line.split('#program')[1].strip()
-        name, deps = signature[:-2].split('(')
-        dep_names = [d for d in deps.split(',') if d]
+        name, deps = parse_signature(line.split('#program')[1].strip()[:-1])
         if name in programs:
             raise Exception("Duplicate test name: " + name)
-        programs[name] = dep_names
+        programs[name] = deps
+        # rewrite into valid ASP (turn functions into plain terms)
+        lines[i] = f"#program {name}({','.join(dep[0] for dep in deps)})."
 
 
 class Tester:
@@ -62,52 +41,39 @@ class Tester:
         self._models_soll = -1
 
     def name(self, name):
-        assrt = Function("assert", [name])
-        if assrt in self._asserts:
-            """ does not work because rules are expanded """
-            #raise Exception("Duplicate assertion name: " + str(name))
-        self._asserts.add(assrt)
+        """ ASP API: add a named assert to be checked for each model """
+        self._asserts.add(clingo.Function("assert", [name]))
         return name
 
     def models(self, n):
-        """ asserts the total number of models found """
+        """ ASP API: add assert for the total number of models """
         self._models_soll = n.number
-        models_term = Function("models", [n])
-        assrt = Function("assert", [models_term])
-        self._asserts.add(assrt)
-        return models_term
+        return self.name(clingo.Function("models", [n]))
 
     def on_model(self, model):
+        """ Callback when model is found; count model and check all asserts. """
         self._models_ist += 1
-        #print("#%s" % self.n, end=': ')
-        for assrt in self._asserts:
-            if not model.contains(assrt):
-                print(model)
-                print("FAILED:", assrt)
-                raise SystemExit
+        failures = [a for a in self._asserts if not model.contains(a)]
+        assert not failures, f"FAILED: {', '.join(map(str, failures))}\nMODEL: {model}"
         return model
 
     def report(self):
-        assert self._models_ist == self._models_soll, self._models_ist
-        print(" %s asserts,  %s models.  OK" % (len(self._asserts), self._models_ist))
+        """ When done, check assert(@models(n)) explicitly, then report. """
+        assert self._models_ist == self._models_soll, f"Expected {self._models_soll} models, found {self._models_ist}."
+        print(f" {len(self._asserts)} asserts,  {self._models_ist} models.  OK")
 
 
-n = 0
 for name, deps in programs.items():
-    tester = Tester()
+    sys.tracebacklimit = 0
+    control = clingo.Control(['0'])
+    control.add('\n'.join(lines))
     if name.startswith('test'):
-        n += 1
         print(name, end=', ', flush=True)
-        control = Control(['0'])
-        control.add(program)
-        testprogram = (name, [Number(1) for _ in deps])
-        depprograms = []
-        for depname in deps:
-            depdeps = programs[depname]
-            depprograms.append((depname, [Number(1) for _ in depdeps]))
+        tester = Tester()
         control.ground(
-                [testprogram] + depprograms,
-                context = tester
+            [(name, [clingo.Number(1) for _ in deps])] +
+            [(depname, [clingo.parse_term(str(a)) for a in depargs]) for depname, depargs in deps],
+            context = tester
         )
         control.solve(on_model=tester.on_model)
         tester.report()
