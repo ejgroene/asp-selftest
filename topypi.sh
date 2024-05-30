d9381826a4e0ba7f2202e9b60f394f70993bea13
Erik Groeneveld <ejgroene@ieee.org>
2024-05-30 10:09:36 +0200

    confirm upload to pypi

--- topypi.sh
+++ topypi.sh
@@ -1,8 +1,11 @@
 LATESTTAG=$(git describe --tags --abbrev=0)
 
-#rm -rf build dist
+rm -rf build dist
 
 python -m build
-#python -m twine upload dist/*
 
-#rm -rf build dist
+read -p "Upload to PyPi? (enter to confirm, ctrl-c to quit)"
+
+python -m twine upload dist/*
+
+rm -rf build dist
